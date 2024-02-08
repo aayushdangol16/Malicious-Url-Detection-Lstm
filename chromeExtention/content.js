0b7addfd5667ddf85beb982315edd8c45b9fb958
aayushdangol16 <83861312+aayushdangol16@users.noreply.github.com>
2024-02-08 14:27:28 +0545

    Delete chromeExtention directory

--- chromeExtention/content.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.result) {
-        console.log("Received result in content script:", request.result);
-        if(request.result=='https://acem.edu.np/'){
-        window.location.href = "https://www.example.com";
-            }    
-        }
-});
